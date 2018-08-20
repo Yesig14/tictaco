abc75089a7f9a38e9b9894bb06ac09ff2280268c
Yesig14 <41810801+Yesig14@users.noreply.github.com>
2018-08-20 01:48:34 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log("Connected");
+console.log(`Connected!`);
+
+function startGame() {
+  document.turn = "X";
+}
+
+
+function nextMove(cell) {
+   if(cell.innerText == '') {
+      cell.innerText = document.turn;
+   switchTurn();
+   }else {
+       clearInterval();
+   }
+}
+
+function switchTurn() {
+  if(document.turn == "X") {
+      document.turn = "O";
+  } else {
+      document.turn = "X";
+  }
+}
+
+
+
+
+
+
+
+ 
+    
+ 
